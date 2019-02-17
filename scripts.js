da611958760555669b0a489b195cf9db17796e39
Karlis Caune <karlis@grandem.lv>
2019-02-18 00:06:34 +0200

    Validation on input field

--- scripts.js
+++ scripts.js
@@ -1,32 +1,42 @@
 // Morakuls
 
+$("#answer-text").keypress(function() {
+    if ($(this).val().length > 1) {
+        $("#answer-text").removeClass("invalid");
+    }
+});
+
 function askMorakul() {
     let question = $("#answer-text").val();
-    let prediction = Math.random();
-    let answer = "";
-    if (prediction <= 0.3) {
-        answer = "😾 Nē";
-    };
-    if (prediction > 0.3 && prediction < 0.7) {
-        answer = "😺  Jā";
-    };
-    if (prediction >= 0.7) {
-        answer = "Mau... 🐈";
-    };
+    if (question.length < 2) {
+        $("#answer-text").addClass("invalid");
+    } else {
+        $("#answer-text").removeClass("invalid");
+        let prediction = Math.random();
+        let answer = "";
+        if (prediction <= 0.3) {
+            answer = "😾 Nē";
+        };
+        if (prediction > 0.3 && prediction < 0.7) {
+            answer = "😺  Jā";
+        };
+        if (prediction >= 0.7) {
+            answer = "Mau... 🐈";
+        };
 
+        $("#question").html(question);
+        $("#response").html(answer);
 
-    $("#question").html(question);
-    $("#response").html(answer);
+        $(".result").addClass("visible");
 
-    $(".result").addClass("visible");
+        setTimeout(function(){ 
+            $(".result").addClass("morra-fly-in");
+        }, 1000);
 
-    setTimeout(function(){ 
-        $(".result").addClass("morra-fly-in");
-    }, 1000);
-
-    setTimeout(function(){ 
-        $(".result").addClass("content-visible");
-    }, 5000);
+        setTimeout(function(){ 
+            $(".result").addClass("content-visible");
+        }, 5000);
+    }
 };
 
 function resetFields() {
